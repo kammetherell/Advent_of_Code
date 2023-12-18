bbb445a137366a502fb40bcdf11ed77390ebd68a
Kevin Metherell <kevin.metherell@experian.com>
2023-12-18 10:20:58 +0000

    add new helper functions

--- aoc_helpers/aoc_helpers/helper.py
+++ aoc_helpers/aoc_helpers/helper.py
@@ -3,3 +3,10 @@ def get_max_row_col(lst):
     max_col = len(lst[0])
 
     return max_row, max_col
+
+def list_of_lists(input):
+    input  = input.split('\n\n')
+    for idx, inp in enumerate(input):
+        input[idx] = inp.splitlines()
+
+    return input
